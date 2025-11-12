9cd406a1a4f665e89d5290345fd45bb8043410cc
Fajla Rabby <fajlarabbyla24@gmail.com>
2025-11-12 20:49:10 +0600

    generics with interface

---
+++ src/video4.ts
@@ -0,0 +1,65 @@
+interface Developer<T, X = null> {
+  name: string;
+  salary: number;
+  device: {
+    brand: string;
+    model: string;
+    releasedYear: string;
+  };
+  smartWatch: T;
+  bike?: X;
+}
+
+interface BrandCharaWatch {
+  heartRate: string;
+  stopwatch: boolean;
+}
+
+interface AppleWatch {
+  heartRate: string;
+  callSupport: boolean;
+  calculator: boolean;
+  AiFeature: boolean;
+}
+
+const poorDeveloper: Developer<
+  BrandCharaWatch,
+  { brand: string; engineCapacity: string }
+> = {
+  name: "Mr. Poor",
+  salary: 20,
+  device: {
+    brand: "lenovo",
+    model: "A21",
+    releasedYear: "2010",
+  },
+  smartWatch: {
+    heartRate: "200",
+    stopwatch: true,
+  },
+  bike: {
+    brand: "yamaha",
+    engineCapacity: "200cc",
+  },
+};
+
+const richDeveloper: Developer<AppleWatch> = {
+  name: "Mr. Rich",
+  salary: 100,
+  device: {
+    brand: "hp",
+    model: "X34",
+    releasedYear: "2050",
+  },
+  smartWatch: {
+    heartRate: "200",
+    callSupport: true,
+    calculator: true,
+    AiFeature: true,
+  },
+  bike: null,
+};
+
+const add = (num1: number, num2: number = 0) => num1 + num2;
+
+add(2);
